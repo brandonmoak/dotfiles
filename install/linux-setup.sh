ea8cd2606965680c94bf6aa039d0ae305db3370b
Ubuntu <brandon.l.moak@gmail.com>
2024-05-23 18:32:43 +0000

    adding linux setup script

--- install/linux-setup.sh
+++ install/linux-setup.sh
@@ -1,2 +1,13 @@
+sudo apt-get install tmux
+sudo apt-get install vim
+sudo apt-get install curl
+sudo apt-get install git
+sudo apt-get install htop
+sudo apt-get install silversearcher-ag
+
+#install fuse & encfs
+sudo apt-get install fuse
+sudo apt-get install encfs
+
 sudo apt install python3-virtualenv
 pip install --upgrade pip
