d555c2dc31f857fef181f16e46ae68213f368418
brandonmoak <developer@bmoak.com>
2025-03-27 12:54:56 -0400

    thinking about changes... Enhance Git configuration and scripts
    - Added custom Git commands for undoing the last commit and generating smart commit messages.
    - Updated user email in `.gitconfig`.
    - Implemented a function in `.rcfunctions` to add include paths to the global Git configuration.
    - Modified `mac-install.sh` to source `.rcfunctions` and add the Git configuration include path.

--- install/mac-install.sh
+++ install/mac-install.sh
@@ -8,4 +8,11 @@ cp -i $SCRIPT_DIR/../dotfiles/.tmux.conf $HOME/.tmux.conf
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "source $SCRIPT_DIR/../dotfiles/mac.bashrc" >> ~/.bashrc
 
+# load the rcfunctions
+ls $SCRIPT_DIR/../dotfiles/.rcfunctions
+source $SCRIPT_DIR/../dotfiles/.rcfunctions
+
+# add an include in the gitconfig
+add_gitconfig_include $SCRIPT_DIR/../dotfiles/.gitconfig
+
 
